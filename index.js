fc94df7b7fcd2cee8131fc39dbb6c274f7c746d1
Veronica Bustabad <vbustabad@gmail.com>
2018-09-07 15:43:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function driversWithRevenueOver(drivers, revenue) {
 }
 
 function driverNamesWithRevenueOver(drivers, revenue) {
-  return drivers.filter(function(driver.name) {
+  return drivers.filter(function(driver) {
     return driver.revenue > revenue;
   });
 }
